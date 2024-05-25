b68885d878aa5a2a67eff100d8436250c568f404
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-05-25 03:43:52 -0400

    database: add map from snake case to camel case in needed models in prisma schema

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,24 +5,15 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
-import DiscordProvider from "next-auth/providers/discord";
 
-import { env } from "@/env";
 import { db } from "@/server/db";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
-      // ...other properties
-      // role: UserRole;
     } & DefaultSession["user"];
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
 export const authOptions: NextAuthOptions = {
